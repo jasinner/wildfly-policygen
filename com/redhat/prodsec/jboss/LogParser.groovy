15c61ae6ba2c25391bf916b87a6316d6af347618
Jason Shepherd <jshepherd@redhat.com>
2016-01-22 14:40:59 +1000

    fixed compatipility issue with Mac/Win

--- com/redhat/prodsec/jboss/LogParser.groovy
+++ com/redhat/prodsec/jboss/LogParser.groovy
@@ -5,10 +5,11 @@ import com.redhat.prodsec.jboss.Permission
 
 class LogParser{
 
-    private static Pattern p = ~/(?i)permission\s"\("(?<clazz>[^"]*+)"\s"(?<name>[^"]*+)"(?:\s"(?<action>[^"]*+)")?\)[\sa-z:\/\("-]*modules\/system\/layers\/base\/(?<module>[a-z\/]*)\/main/
+    private static Pattern p = ~/(?i)permission\s"\("(?<clazz>[^"]*+)"\s"(?<name>[^"]*+)"(?:\s"(?<action>[^"]*+)")?\)[\sa-zA-Z:\/\("-]*modules\/system\/layers\/base\/(?<module>[a-z\/]*)\/main/
     private Matcher m = null
 
     static{
+        testRegex('2016-01-22 14:05:32,958 DEBUG [org.wildfly.security.access] (MSC service thread 1-4) Permission check failed (permission "("org.jboss.as.server.security.ServerPermission" "setCurrentServiceContainer")" in code source "(jar:file:/Users/jasonshepherd/products/middleware-product-distributions/modules/system/layers/base/org/jboss/msc/main/jboss-msc-1.2.6.Final-redhat-1.jar!/ <no signer certificates>)" of "null")')
         testRegex('(permission "("java.lang.RuntimePermission" "setClassLoader")" in code source "(jar:file:/home/jshepher/apps/product-distributions/modules/system/layers/base/org/jboss/as/controller/main/wildfly-controller-2.0.3.Final-redhat-1.jar!/')
         testRegex('2016-01-12 12:38:47,439 DEBUG [org.wildfly.security.access] (Controller Boot Thread) Permission check failed (permission "("java.lang.RuntimePermission" "getClassLoader")" in code source "(jar:file:/home/jshepher/apps/product-distributions/modules/system/layers/base/org/jboss/as/controller/main/wildfly-controller-2.0.3.Final-redhat-1.jar!/ <no signer certificates>)" of "ModuleClassLoader for Module "org.jboss.as.controller:main" from local module loader @47089e5f (finder: local module finder @4141d797 (roots: /home/jshepher/apps/product-distributions/modules,/home/jshepher/apps/product-distributions/modules/system/layers/base))")')
         testRegex('2016-01-12 12:38:46,566 DEBUG [org.wildfly.security.access] (Periodic Recovery) Permission check failed (permission "("java.io.FilePermission" "/home/jshepher/apps/product-distributions/standalone/data/tx-object-store/ShadowNoFileLockStore/defaultStore" "read")" in code source "(jar:file:/home/jshepher/apps/product-distributions/modules/system/layers/base/org/jboss/as/transactions/main/wildfly-transactions-10.0.0.CR6-redhat-1.jar!/ <no signer certificates>)" of "null")')
